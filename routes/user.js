70f83b7d1baabdb7384f020bee5c4f21f32bbb21
beluyencode <81684179+beluyencode@users.noreply.github.com>
2021-12-02 00:50:13 +0700

    fix name file

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../controller/controllerUser')
+const User = require('../controller/userController')
 
 
 router.get('/',User.get);
